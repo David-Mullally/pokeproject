9062d4f884fa26b8029acd7852b4530cb1396842
David Mullally <mullallydavid7@gmail.com>
2022-10-07 00:06:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let count = 648;
+
+function scrollThroughImages(){
+let pokedex = document.getElementById('viewer-window');
+    pokedex.innerHTML = `<img class="pokepic" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
+}
+
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+prev.onclick = function () {
+    if (count > 1){
+    count = count - 1;
+        scrollThroughImages();
+        }
+}
+
+next.onclick = function () {
+    if (count < 649){
+    count = count + 1;
+        scrollThroughImages();
+        }
+}
+
+scrollThroughImages();
